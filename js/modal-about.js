4d2186ee4e519c1b45c2965c57e922c875d72948
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-03-29 13:59:35 +0200

    feature/add video & franchise modal responsive

--- js/modal-about.js
+++ js/modal-about.js
@@ -3,10 +3,14 @@
     openModalBtn: document.querySelector("[data-modal-about-open]"),
     closeModalBtn: document.querySelector("[data-modal-about-close]"),
     modal: document.querySelector("[data-modal-about]"),
+    modalVideo: document.querySelector("[data-modal-about-video]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", () => {
+    toggleModal();
+    refs.modalVideo.pause();
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
